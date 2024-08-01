fe9d507391d6df5909b95d47f7ac4a1a46857d76
Juan Rendon <juanprendon2603@gmail.com>
2024-07-31 23:00:11 -0500

    noticias y contactenos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default {
-    output: 'server', // Alternativamente, puedes usar 'hybrid'
-  };
+export default defineConfig({
+  integrations: [react()],
+});
--- src/data/news.js
+++ src/data/news.js
@@ -1,27 +1,97 @@
 // src/data/news.js
 export const news = [
-    {
-      id: '1',
-      title: 'Título de la Noticia 1',
-      description: 'Descripción corta de la noticia 1.',
-      image: '/images/image.png',
-      content: 'Contenido completo de la noticia 1...'
-    },
-    {
-      id: '2',
-      title: 'Título de la Noticia 2',
-      description: 'Descripción corta de la noticia 2.',
-      image: '/images/image.png',
-      content: 'Contenido completo de la noticia 2...'
-    },
-    // Más noticias aquí
-  ];
-  
-  export function getNews() {
-    return news;
-  }
-  
-  export function getNewsById(id) {
-    return news.find(item => item.id === id) || null; // Retorna null si no se encuentra la noticia
-  }
-  
+  {
+    id: "1",
+    title: "Título de la Noticia 1",
+    description: "Descripción corta de la noticia 1.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 1...",
+  },
+  {
+    id: "2",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "3",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "4",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "5",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "6",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "7",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "8",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "9",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "10",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "11",
+    title: "Título de la Noticia 2",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+  {
+    id: "12",
+    title: "Título de la Noticia ultima",
+    description: "Descripción corta de la noticia 2.",
+    image: "/images/image.png",
+    content: "Contenido completo de la noticia 2...",
+  },
+
+  // Más noticias aquí
+];
+
+export function getNews() {
+  return news;
+}
+
+export function getNewsById(id) {
+  return news.find((item) => item.id === id) || null; // Retorna null si no se encuentra la noticia
+}
